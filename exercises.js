70f13ac63531f388c3d48baeb63ff4b848fee2ac
Krzysztof Kaczmarek <1krzysztof.kaczmarek@gmail.com>
2022-12-01 17:09:09 +0100

    first commit

---
+++ exercises.js
@@ -0,0 +1,24 @@
+//isPalindorne-of course if we give an input
+function isPalindrone(word) {
+	if (word.length === 1 || word.length === 0) {
+		return true
+	}
+	if (word[0] === word[word.length - 1]) {
+		return isPalindrone(word.substring(1, word.length - 1))
+	}
+	return false
+}
+// console.log(isPalindrone("a")) //true
+// console.log(isPalindrone("ab")) //false
+// console.log(isPalindrone("racecar")) //true
+//recursion-of course if we give a word or even letter
+function reveRswoRd(word){
+    if(word.length===1){
+        return word
+    } else {
+        return reveRswoRd(word.substring(1)) + word.charAt(0)
+    }
+}
+// console.log(reveRswoRd("java script"))//tpircs avaj
+// console.log(reveRswoRd("ab"));
+// console.log(reveRswoRd("a"));
