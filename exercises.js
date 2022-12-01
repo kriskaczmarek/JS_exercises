f66889f2188771bd0911344be1810d30403e526c
Krzysztof Kaczmarek <1krzysztof.kaczmarek@gmail.com>
2022-12-01 18:07:00 +0100

    second commit

--- exercises.js
+++ exercises.js
@@ -12,13 +12,22 @@ function isPalindrone(word) {
 // console.log(isPalindrone("ab")) //false
 // console.log(isPalindrone("racecar")) //true
 //recursion-of course if we give a word or even letter
-function reveRswoRd(word){
-    if(word.length===1){
-        return word
-    } else {
-        return reveRswoRd(word.substring(1)) + word.charAt(0)
-    }
+function reveRswoRd(word) {
+	if (word.length === 1) {
+		return word
+	} else {
+		return reveRswoRd(word.substring(1)) + word.charAt(0)
+	}
 }
 // console.log(reveRswoRd("java script"))//tpircs avaj
 // console.log(reveRswoRd("ab"));
 // console.log(reveRswoRd("a"));
+
+//recursion sum of natural numbers
+function sumNaturalNumbers(number) {
+	if (number <= 1) {
+		return number
+	}
+	return number + sumNaturalNumbers(number - 1)
+}
+console.log(sumNaturalNumbers(10));
